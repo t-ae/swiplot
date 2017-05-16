1a6dd2d86c5a1b697ed742b218268262ebc1923a
araki <araki@qoncept.co.jp>
2017-05-16 17:32:10 +0900

    Add `plot`

--- Sources/SingleLine.swift
+++ Sources/SingleLine.swift
@@ -8,6 +8,16 @@ public func plot(_ values: [Double]) {
     plot.plot()
 }
 
+public func plot(x: [Double], y: [Double]) {
+    let graph = Graph(x: x, y: y)
+    graph.lineStyle = .lines
+    
+    let plot = Plot()
+    
+    plot.addGraph(graph)
+    plot.plot()
+}
+
 public func scatter(_ data: [(x: Double, y: Double)]) {
     let graph = Graph(data)
     graph.lineStyle = .points
