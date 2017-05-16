63603fa04c2feb4f8c57a1ec4fc29b24118a6b10
araki <araki@qoncept.co.jp>
2017-05-16 09:26:34 +0900

    Initial commit

---
+++ Sources/Swiplot.swift
@@ -0,0 +1,165 @@
+
+import Foundation
+
+public class Plot {
+    
+    public var setting = Setting()
+    
+    var graphs: [Graph] = []
+    
+    public func plot(gnuplotPath: String? = nil) {
+        let process = Process()
+        
+        if let gnuplotPath = gnuplotPath {
+            process.launchPath = gnuplotPath
+            process.arguments = ["-persist"]
+        } else {
+            process.environment = ProcessInfo.processInfo.environment
+            process.environment!["PATH"] = "/usr/local/bin:" + process.environment!["PATH"]!
+            process.launchPath = "/usr/bin/env"
+            
+            process.arguments = ["gnuplot", "-persist"]
+        }
+        
+        let stdin = Pipe()
+        let stdout = Pipe()
+        let stderr = Pipe()
+        
+        func query(_ str: String) {
+            print(str)
+            let str = str + "\n"
+            stdin.fileHandleForWriting.write(str.data(using: .utf8)!)
+        }
+        
+        process.standardInput = stdin
+        process.standardOutput = stdout
+        process.standardError = stderr
+        
+        stdout.fileHandleForReading.readabilityHandler = { h in
+            print(String(data: h.availableData, encoding: .utf8)!)
+        }
+        stderr.fileHandleForReading.readabilityHandler = { h in
+            print(String(data: h.availableData, encoding: .utf8)!)
+        }
+        
+        process.launch()
+        
+        for q in setting.queries() {
+            query(q)
+        }
+        
+        let header = "plot " + graphs.map { $0.headerQuery() }.joined(separator: ", ")
+        query(header)
+        for graph in graphs {
+            for q in graph.dataQueries() {
+                query(q)
+            }
+        }
+        query("exit")
+    }
+    
+    public func addGraph(_ graph: Graph) {
+        graphs.append(graph)
+    }
+}
+
+public struct Setting {
+    public var xlabel: String?
+    public var ylabel: String?
+    
+    public var xrange: (Double, Double)?
+    public var yrange: (Double, Double)?
+    
+    public var pointsize: Double?
+    
+    func queries() -> [String] {
+        var ret: [String] = []
+        
+        if let xlabel = self.xlabel {
+            ret.append("set xlabel '\(xlabel)'")
+        }
+        if let ylabel = self.ylabel {
+            ret.append("set ylabel '\(ylabel)'")
+        }
+        
+        if let xrange = self.xrange {
+            ret.append("set xrange [\(xrange.0):\(xrange.1)]")
+        }
+        if let yrange = self.yrange {
+            ret.append("set yrange [\(yrange.0):\(yrange.1)]")
+        }
+        
+        if let pointsize = self.pointsize {
+            ret.append("set pointsize \(pointsize)")
+        }
+        
+        return ret
+    }
+}
+
+public protocol Graph {
+    func headerQuery() -> String
+    func dataQueries() -> [String]
+}
+
+public struct Scatter: Graph {
+    let title: String
+    let data: [(x: Double, y: Double)]
+    let color: Color?
+    
+    public init(title: String, data: [(x: Double, y: Double)], color: Color? = nil) {
+        self.title = title
+        self.data = data
+        self.color = color
+    }
+    
+    public func headerQuery() -> String {
+        var ret = "'-' u 1:2 title '\(title)'"
+        
+        if let color = self.color {
+            ret += " lc rgb '\(color)'"
+        }
+        
+        return ret
+    }
+    
+    public func dataQueries() -> [String] {
+        return data.map { x, y in "\(x) \(y)" } + ["e"]
+    }
+}
+
+public struct Line: Graph {
+    let title: String
+    let data: [(x: Double, y: Double)]
+    let color: Color?
+    
+    init(title: String, data: [(x: Double, y: Double)], color: Color? = nil) {
+        self.title = title
+        self.data = data
+        self.color = color
+    }
+    
+    public func headerQuery() -> String {
+        var ret = "'-' u 1:2 w line title '\(title)'"
+        
+        if let color = self.color {
+            ret += " lc rgb '\(color)'"
+        }
+        
+        return ret
+    }
+    
+    public func dataQueries() -> [String] {
+        return data.map { x, y in "\(x) \(y)" } + ["e"]
+    }
+}
+
+public enum Color: String {
+    case red = "red"
+    case green = "green"
+    case blue = "blue"
+    case black = "black"
+    case cyan = "cyan"
+    case magenta = "magenta"
+    case yellow = "yellow"
+}
---
+++ Tests/SwiplotTests/SwiplotTests.swift
@@ -0,0 +1,25 @@
+import XCTest
+@testable import Swiplot
+
+class SwiplotTests: XCTestCase {
+    #if !SWIFT_PACKAGE
+    func testExample() {
+        let plot = Plot()
+        
+        plot.setting.xrange = (-10, 10)
+        plot.setting.xlabel = "hoge"
+        plot.setting.pointsize = 3
+        
+        let scatter1 = Scatter(title: "scatter1", data: [(1, 2), (2, 1), (3, 5), (4, 1), (2, 3)], color: .black)
+        plot.addGraph(scatter1)
+        
+        let scatter2 = Scatter(title: "scatter2", data: [(3, 3), (1,5), (-1, 2), (4, 2)], color: .cyan)
+        plot.addGraph(scatter2)
+        
+        let line = Line(title: "line", data: [(0, 0), (1, 2), (2, 1), (3, 3)], color: .red)
+        plot.addGraph(line)
+        
+        plot.plot()
+    }
+    #endif
+}
